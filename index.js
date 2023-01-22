03bdb0dad40f0ad8b3026facb697947eb6419642
sharath2003chary <119683683+sharath2003chary@users.noreply.github.com>
2023-01-22 14:04:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+var n=0;
+var noOfDrumbuttons = document.querySelectorAll(".drum").length;
+for (n = 0; n < noOfDrumbuttons; n++) {
+    document.querySelectorAll(".drum")[n].addEventListener("click", play);
+
+
+    // function play(){
+    //     this.style.color="white"
+    //     alert("hi");
+    // }
+    function play() {
+        var buttonInnerHTML = this.innerHTML;
+        switch (buttonInnerHTML) {
+            case "w":
+                var tom1 = new Audio("sounds/tom-1.mp3");
+                tom1.play();
+
+                break;
+            case "a":
+                var tom2 = new Audio("sounds/tom-2.mp3");
+                tom2.play();
+
+                break;
+            case "s":
+                var tom3 = new Audio("sounds/tom-3.mp3");
+                tom3.play();
+
+                break;
+            case "d":
+                var tom4 = new Audio("sounds/tom-4.mp3");
+                tom4.play();
+
+                break;
+            case "j":
+                var snare = new Audio("sounds/snare.mp3");
+                snare.play();
+
+                break;
+            case "k":
+                var crash = new Audio("sounds/crash.mp3");
+                crash.play();
+
+                break;
+            case "l":
+                var kick = new Audio("sounds/kick.mp3");
+                kick.play();
+
+                break;
+
+            default:
+                console.log(buttonInnerHTML);
+        }
+
+
+    }
+ }
